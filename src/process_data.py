a377de638a29ed2d6c808f05e3f8984efe1601f3
cchen120 <catherine_chen@brown.edu>
2022-11-21 16:52:32 -0500

    added shooting data

--- src/process_data.py
+++ src/process_data.py
@@ -7,15 +7,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os.path
 
-f = open('NYPD_Calls_for_Service__Year_to_Date_.csv', 'r', encoding="utf8")
+f = open('NYPD_Calls_for_Service__Historic_.csv', 'r', encoding="utf8")
 
 chunksize = 5000
 for chunk in pd.read_csv(f, chunksize = chunksize):
     chunk['INCIDENT_DATE'] = pd.to_datetime(chunk['INCIDENT_DATE'])
-    chunk = chunk[(chunk['INCIDENT_DATE'] < '10/01/2022') & (chunk['INCIDENT_DATE'] > '09/22/2022')]
+    chunk = chunk[(chunk['INCIDENT_DATE'] < '01/01/2020') & (chunk['INCIDENT_DATE'] > '12/31/2018')]
     
-    if(os.path.isfile('processed_crime_data.csv')):
-        chunk.to_csv('processed_crime_data.csv', encoding='utf-8', mode='a', index=False, header=False)
+    if(os.path.isfile('processed_crime_data_2019.csv')):
+        chunk.to_csv('processed_crime_data_2019.csv', encoding='utf-8', mode='a', index=False, header=False)
     else:
-        chunk.to_csv('processed_crime_data.csv', encoding='utf-8', index=False)
+        chunk.to_csv('processed_crime_data_2019.csv', encoding='utf-8', index=False)
+
+# f = open('NYPD_Shooting_Incident_Data__Historic_.csv', 'r', encoding="utf8")
+
+# chunksize = 5000
+# for chunk in pd.read_csv(f, chunksize = chunksize):
+#     chunk['OCCUR_DATE'] = pd.to_datetime(chunk['OCCUR_DATE'])
+#     chunk = chunk[(chunk['OCCUR_DATE'] < '01/01/2020') & (chunk['OCCUR_DATE'] > '12/31/2018')]
+    
+#     if(os.path.isfile('processed_shooting_data_2019.csv')):
+#         chunk.to_csv('processed_shooting_data_2019.csv', encoding='utf-8', mode='a', index=False, header=False)
+#     else:
+#         chunk.to_csv('processed_shooting_data_2019.csv', encoding='utf-8', index=False)
         
