d4b56622d7a124b9d71f406daf7977e98326a4f7
cchen120 <catherine_chen@brown.edu>
2022-11-22 13:29:48 -0500

    sorted merge

--- src/process_data.py
+++ src/process_data.py
@@ -7,17 +7,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os.path
 
-f = open('NYPD_Calls_for_Service__Historic_.csv', 'r', encoding="utf8")
+# f = open('NYPD_Calls_for_Service__Historic_.csv', 'r', encoding="utf8")
 
-chunksize = 5000
-for chunk in pd.read_csv(f, chunksize = chunksize):
-    chunk['INCIDENT_DATE'] = pd.to_datetime(chunk['INCIDENT_DATE'])
-    chunk = chunk[(chunk['INCIDENT_DATE'] < '01/01/2020') & (chunk['INCIDENT_DATE'] > '12/31/2018')]
+# chunksize = 5000
+# for chunk in pd.read_csv(f, chunksize = chunksize):
+#     chunk['INCIDENT_DATE'] = pd.to_datetime(chunk['INCIDENT_DATE'])
+#     chunk = chunk[(chunk['INCIDENT_DATE'] < '01/01/2020') & (chunk['INCIDENT_DATE'] > '12/31/2018')]
     
-    if(os.path.isfile('processed_crime_data_2019.csv')):
-        chunk.to_csv('processed_crime_data_2019.csv', encoding='utf-8', mode='a', index=False, header=False)
-    else:
-        chunk.to_csv('processed_crime_data_2019.csv', encoding='utf-8', index=False)
+#     if(os.path.isfile('processed_crime_data_2019.csv')):
+#         chunk.to_csv('processed_crime_data_2019.csv', encoding='utf-8', mode='a', index=False, header=False)
+#     else:
+#         chunk.to_csv('processed_crime_data_2019.csv', encoding='utf-8', index=False)
 
 # f = open('NYPD_Shooting_Incident_Data__Historic_.csv', 'r', encoding="utf8")
 
@@ -30,4 +30,10 @@ for chunk in pd.read_csv(f, chunksize = chunksize):
 #         chunk.to_csv('processed_shooting_data_2019.csv', encoding='utf-8', mode='a', index=False, header=False)
 #     else:
 #         chunk.to_csv('processed_shooting_data_2019.csv', encoding='utf-8', index=False)
+
+f = open('merged.csv', 'r', encoding="utf8")
+df = pd.read_csv(f)
+df.sort_values(by=['borough', 'date'], inplace=True)
+df.drop(['Unnamed: 0'], axis=1, inplace=True)
+df.to_csv('merged_sorted.csv',  encoding='utf-8', index=True)
         
