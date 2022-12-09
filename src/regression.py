0e7b67353f302a974debd71ebc43a7824971b462
Sheri Zhang <sherizhang83@gmail.com>
2022-12-09 14:19:49 -0500

    sid's congestion metric

---
+++ src/regression.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import statsmodels.api as sm
+
+df = pd.read_csv("data/raw/body.csv")
+print(type(df['daytime_routes'][0]))
+df['lines']=df['daytime_routes'].apply(lambda x: len(str(x).split(' ')))
+df['congestion'] = df['entries'].div(df['lines'])
+print(df['congestion'].head(10))
+df.to_csv("congestion.csv")
