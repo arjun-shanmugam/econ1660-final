bf867f3f7cfadd1687259385c81487ee06799ebd
arjun-shanmugam <65803362+arjun-shanmugam@users.noreply.github.com>
2022-12-09 15:16:28 -0500

    Ran FE reressions

--- src/crimebyperson.py
+++ src/crimebyperson.py
@@ -4,27 +4,25 @@ import numpy as np
 
 
 station_data = pd.read_csv("../data/clean/subway_crime_panel.csv")
+station_data.loc[:, 'month'] = pd.to_datetime(station_data['Date']).dt.month
+station_data = station_data.set_index(['stop_name', 'month'])
+stop_name_month_means = station_data.groupby(['stop_name', 'month'])[['entries', 'exits']].mean().rename(columns={'entries': 'mean_entries',
+                                                                                                                  'exits': 'mean_exits'})
+station_data = pd.concat([station_data, stop_name_month_means], axis=1)
+station_data.loc[:, 'demeaned_entries'] = station_data['entries'] - station_data['mean_entries']
+station_data.loc[:, 'demeaned_exits'] = station_data['exits'] - station_data['mean_exits']
 
-unique_stations = station_data.stop_name.unique().tolist()
-
-# regress incidents on number of exits for 1st ave station only
-first_ave = station_data.loc[station_data.stop_name==unique_stations[0]]
-X = np.log(first_ave['exits'] +1) 
-y = np.log(first_ave['incidents'] +1)
-model = sm.OLS(y, sm.add_constant(X)).fit()
-print("first ave station only log exits + log incidents:")
-print(model.summary()) 
 
 # regress incidents on number of exits
-X = np.log(station_data['exits']+1)
-y = np.log(station_data['incidents']+1)
+X = station_data['demeaned_exits']
+y = station_data['incidents']
 model = sm.OLS(y, sm.add_constant(X)).fit()
 print("overall log exits + log incidents:")
 print(model.summary()) 
 
 # regress incidents on number of entries
-X = np.log(station_data['entries']+1)
-y = np.log(station_data['incidents']+1)
+X = station_data['demeaned_entries']
+y = station_data['incidents']
 model = sm.OLS(y, sm.add_constant(X)).fit()
 print("overall log entries + log incidents:")
 print(model.summary()) 
